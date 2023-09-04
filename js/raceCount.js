ca34e4d89ae0bf1847070478c541b0b1556f40a2
Clement Lazzarini <clement.lazzarini71@gmail.com>
2023-09-04 10:42:56 +0200

    MAJ04092023

--- js/raceCount.js
+++ js/raceCount.js
@@ -2,14 +2,14 @@ function getRaceCount(){
     fetch("https://ergast.com/api/f1/current/last/results.json")
     .then((response) => response.json())
     .then(({ MRData: { RaceTable } }) => {
-        //console.log(RaceTable)
+        console.log(RaceTable)
         const season = (RaceTable.season);
         let compteurCourses = document.getElementById("racecount");
         let baseNBCourses = document.getElementById("basenbcourse").textContent;
         const baseNBCoursesInt = parseInt(baseNBCourses, 10);
         if(season == "2022"){
             compteurCourses.textContent = baseNBCoursesInt;
-        } else if (season = "2023"){
+        } else if (season == "2023"){
             const lastRound = RaceTable.Races[0].round;
             const lastRoundInt = parseInt(lastRound, 10);
             
